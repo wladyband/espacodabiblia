fe0012eee58a6c2109770dbc361414a8414d1bbd
Wladimir Bandeira <tecnico100@gmail.com>
2022-12-08 13:48:43 -0300

    criando a classe controladora de usuários e realizando pequenas alterações na classe de serviços

--- back/src/modules/users/services/CreateUserService.ts
+++ back/src/modules/users/services/CreateUserService.ts
@@ -6,12 +6,13 @@ import UsersRepository from '../typeorm/repositories/UsersRepository';
 
 interface IRequest {
   name: string;
+  telephone: string;
   email: string;
   password: string;
 }
 
 class CreateUserService {
-  public async execute({ name, email, password }: IRequest): Promise<User> {
+  public async execute({ name, email, telephone, password }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
     const emailExists = await usersRepository.findByEmail(email);
 
@@ -19,13 +20,14 @@ class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    
 
 
     const user = usersRepository.create({
       name,
+      telephone,
       email,
-      password: hashedPassword,
+      password,
     });
 
     await usersRepository.save(user);
