e2a19f72080d049b8a385f661024e03580392bcc
Wladimir Bandeira <tecnico100@gmail.com>
2022-12-08 14:05:53 -0300

    criando a rota do usuário e o controller

--- back/src/modules/users/controllers/UsersController.ts
+++ back/src/modules/users/controllers/UsersController.ts
@@ -12,12 +12,13 @@ export default class UsersController {
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    const { name, email, password } = request.body;
+    const { name, email, telephone, password } = request.body;
 
     const createUser = new CreateUserService();
 
     const user = await createUser.execute({
       name,
+      telephone,
       email,
       password,
     });
--- back/src/shared/http/routes/index.ts
+++ back/src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import questionsRouter from '@modules/questions/routes/questions.routes';
+import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/questions', questionsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
