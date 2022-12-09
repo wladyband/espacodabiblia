93693c69a2c31122f66f14cc436cc660822119be
Wladimir Bandeira <tecnico100@gmail.com>
2022-12-09 09:37:22 -0300

    configurando jwt no back-end

--- back/src/shared/http/routes/index.ts
+++ back/src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import questionsRouter from '@modules/questions/routes/questions.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
@@ -6,5 +7,6 @@ const routes = Router();
 
 routes.use('/questions', questionsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
