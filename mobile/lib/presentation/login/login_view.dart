b238b3558c71fcaaeef79c3c8566cc00493774cb
Wladimir Bandeira <tecnico100@gmail.com>
2022-12-12 16:07:42 -0300

    iniciando configurações da tela de interface gráfica do login do front-end

--- mobile/lib/presentation/login/login_view.dart
+++ mobile/lib/presentation/login/login_view.dart
@@ -1,17 +1,43 @@
-
-import 'package:biblia/presentation/resources/color_manager.dart';
+import 'package:biblia/presentation/login/login_viewmodel.dart';
+import 'package:complete_advanced_flutter/presentation/login/login_viewmodel.dart';
 import 'package:flutter/material.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
 
   @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  LoginViewModel _viewModel =
+  LoginViewModel(null); // todo pass here login useCase
+
+  TextEditingController _userNameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  _bind() {
+    _viewModel.start();
+    _userNameController
+        .addListener(() => _viewModel.setUserName(_userNameController.text));
+    _passwordController
+        .addListener(() => _viewModel.setPassword(_passwordController.text));
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorManager.primary,
-      body: const Center(
-        child: Text("Bem vindo ao Login de acesso"),
-      ),
-    );
+    return Container();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
