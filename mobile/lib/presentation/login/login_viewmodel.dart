ee00dadb1e6b0de38d843e1fbc530d7fbd66485f
Wladimir Bandeira <tecnico100@gmail.com>
2022-12-12 16:05:44 -0300

    iniciando configurações básicas do controller do login

---
+++ mobile/lib/presentation/login/login_viewmodel.dart
@@ -0,0 +1,106 @@
+import 'dart:async';
+
+import 'package:biblia/domain/usecase/login_usecase.dart';
+import 'package:biblia/presentation/base/base_view_model.dart';
+import '/presentation/common/freezed_data_classes.dart';
+
+
+class LoginViewModel extends BaseViewModel
+    with LoginViewModelInputs, LoginViewModelOutputs {
+  StreamController _userNameStreamController =
+  StreamController<String>.broadcast();
+  StreamController _passwordStreamController =
+  StreamController<String>.broadcast();
+
+  var loginObject = LoginObject("", "");
+
+  LoginUseCase? _loginUseCase; // todo remove ?
+
+  LoginViewModel(this._loginUseCase);
+
+  // inputs
+  @override
+  void dispose() {
+    _userNameStreamController.close();
+    _passwordStreamController.close();
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  Sink get inputPassword => _passwordStreamController.sink;
+
+  @override
+  Sink get inputUserName => _userNameStreamController.sink;
+
+  @override
+  login() async {
+    (await _loginUseCase?.execute(
+        LoginUseCaseInput(loginObject.userName, loginObject.password)))
+        ?.fold(
+            (failure) => {
+          // left -> failure
+          print(failure.message)
+        },
+            (data) => {
+          // right -> success (data)
+          print(data.name)
+        });
+  }
+
+  @override
+  setPassword(String password) {
+    inputPassword.add(password);
+    loginObject = loginObject.copyWith(
+        password: password); // data class operation same as kotlin
+  }
+
+  @override
+  setUserName(String userName) {
+    inputUserName.add(userName);
+    loginObject = loginObject.copyWith(
+        userName: userName); // data class operation same as kotlin
+  }
+
+  // outputs
+  @override
+  Stream<bool> get outputIsPasswordValid => _passwordStreamController.stream
+      .map((password) => _isPasswordValid(password));
+
+  @override
+  Stream<bool> get outputIsUserNameValid => _userNameStreamController.stream
+      .map((userName) => _isUserNameValid(userName));
+
+  // private functions
+
+  bool _isPasswordValid(String password) {
+    return password.isNotEmpty;
+  }
+
+  bool _isUserNameValid(String userName) {
+    return userName.isNotEmpty;
+  }
+}
+
+abstract class LoginViewModelInputs {
+  // three functions for actions
+  setUserName(String userName);
+
+  setPassword(String password);
+
+  login();
+
+// two sinks for streams
+  Sink get inputUserName;
+
+  Sink get inputPassword;
+}
+
+abstract class LoginViewModelOutputs {
+  Stream<bool> get outputIsUserNameValid;
+
+  Stream<bool> get outputIsPasswordValid;
+}
