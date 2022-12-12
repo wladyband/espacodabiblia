2bdc2bd0b26ae451137f8beeb81c64dabb2bc1b1
Wladimir Bandeira <tecnico100@gmail.com>
2022-12-12 14:23:52 -0300

    criando configurações para identificar o dispositivo fisico que acessou o sistema front-end

--- mobile/lib/domain/model/model.dart
+++ mobile/lib/domain/model/model.dart
@@ -6,3 +6,19 @@ class Authentication {
 
   Authentication(this.id, this.name, this.email, this.phone);
 }
+
+class DeviceInfo {
+  String name;
+  String identifier;
+  String version;
+
+  DeviceInfo(this.name, this.identifier, this.version);
+}
+
+class Service {
+  int id;
+  String title;
+  String image;
+
+  Service(this.id, this.title, this.image);
+}
---
+++ mobile/lib/domain/usecase/login_usecase.dart
@@ -0,0 +1,27 @@
+import 'package:biblia/app/functions.dart';
+import 'package:biblia/data/network/failure.dart';
+import 'package:biblia/data/request/request.dart';
+import 'package:biblia/domain/model/model.dart';
+import 'package:biblia/domain/repository/repository.dart';
+import 'package:biblia/domain/usecase/base_usecase.dart';
+import 'package:dartz/dartz.dart';
+
+class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
+  Repository _repository;
+
+  LoginUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, Authentication>> execute(LoginUseCaseInput input) async {
+    DeviceInfo deviceInfo = await getDeviceDetails();
+    return await _repository
+        .login(LoginRequest(input.email, input.password, deviceInfo.identifier, deviceInfo.name));
+  }
+}
+
+class LoginUseCaseInput {
+  String email;
+  String password;
+
+  LoginUseCaseInput(this.email, this.password);
+}
